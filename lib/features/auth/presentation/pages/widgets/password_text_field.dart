5d5c3bac813e31e2f668284c0cff380d5d6a01ee
Saurabh Yadav <79056681+Saurrabhh@users.noreply.github.com>
2026-06-07 12:46:52 +0530

    sky lint package fix (#107)

--- lib/features/auth/presentation/pages/widgets/password_text_field.dart
+++ lib/features/auth/presentation/pages/widgets/password_text_field.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:sky_architecture/sky_architecture.dart';
-import 'package:sky_design_system/sky_design_system.dart' show AppTextField;
+import 'package:sky_design_system/sky_design_system.dart'
+    show AppIconButton, AppTextField;
 import 'package:splittr/utils/extensions/l10n_extensions.dart';
 
-class PasswordTextField extends StatelessWidget {
+class PasswordTextField extends StatefulWidget {
   const PasswordTextField({required this.onChanged, super.key});
 
   final ValueChanged<String> onChanged;
 
   @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  late final ValueNotifier<bool> _obscureText;
+
+  @override
+  void initState() {
+    _obscureText = ValueNotifier(true);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AppTextField(
-      labelText: context.strings.password,
-      hintText: context.strings.passwordHintText,
-      obscureText: true,
-      onChanged: onChanged,
-      validator: (password) {
-        if (password?.isNotEmpty ?? false) {
-          return Password(
-            password ?? '',
-          ).value.fold((failure) => failure.message, (_) => null);
-        }
-        return null;
+    return ValueListenableBuilder(
+      valueListenable: _obscureText,
+      builder: (context, obscureText, _) {
+        return AppTextField(
+          labelText: context.strings.password,
+          hintText: context.strings.passwordHintText,
+          obscureText: obscureText,
+          onChanged: widget.onChanged,
+          suffixIcon: AppIconButton(
+            icon: obscureText ? Icons.visibility : Icons.visibility_off,
+            onPressed: () {
+              _obscureText.value = !obscureText;
+            },
+          ),
+          validator: (password) {
+            if (password?.isNotEmpty ?? false) {
+              return Password(
+                password ?? '',
+              ).value.fold((failure) => failure.message, (_) => null);
+            }
+            return null;
+          },
+        );
       },
     );
   }
